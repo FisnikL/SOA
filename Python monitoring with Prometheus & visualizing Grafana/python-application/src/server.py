7c5df7e673dfe7c4541a93527c47806dc9fbbd1d
FisnikL <fisnik.limani1@gmail.com>
2020-03-23 04:12:51 +0100

    add python monitoring app

---
+++ Python monitoring with Prometheus & visualizing Grafana/python-application/src/server.py
@@ -0,0 +1,39 @@
+import prometheus_client
+from prometheus_client import Summary, Counter, Histogram, Gauge
+import time
+from fastapi import FastAPI, Response
+from fastapi.responses import PlainTextResponse
+
+app = FastAPI()
+
+_INF = float("inf")
+
+graphs = {}
+graphs['c'] = Counter('python_request_operations_total', 'The total number of processed requests')
+graphs['h'] = Histogram('python_request_duration_seconds', 'Histogram for the duration in seconds.',
+                        buckets=(1, 2, 5, 6, 10, _INF))
+
+
+@app.get("/")
+def hello():
+    start = time.time()
+    graphs['c'].inc()
+
+    time.sleep(0.600)
+    end = time.time()
+    graphs['h'].observe(end - start)
+    return "Hello World!"
+
+
+@app.get("/metrics", response_class=PlainTextResponse)
+def requests_count():
+    res = []
+    for k, v in graphs.items():
+        res.append(prometheus_client.generate_latest(v).decode("utf-8"))
+
+    return ''.join(res)
+
+
+
+
+
