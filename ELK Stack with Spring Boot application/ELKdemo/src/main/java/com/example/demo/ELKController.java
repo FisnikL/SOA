a0802d6bbbbdc45d06f91e57692af91b38d057a7
FisnikL <fisnik.limani1@gmail.com>
2020-04-05 23:08:12 +0200

    final dockerizing

---
+++ ELK Stack with Spring Boot application/ELKdemo/src/main/java/com/example/demo/ELKController.java
@@ -0,0 +1,53 @@
+package com.example.demo;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Date;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+class ELKController {
+    private static final Logger LOG = LogManager.getLogger(ELKController.class.getName());
+
+    @Autowired
+    RestTemplate restTemplete;
+
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @RequestMapping(value = "/elk")
+    public String helloWorld() {
+        String response = "Welcome to JavaInUse" + new Date();
+        LOG.log(Level.INFO, response);
+
+        return response;
+    }
+
+    @RequestMapping(value = "/exception")
+    public String exception() {
+        String response = "";
+        try {
+            throw new Exception("Exception has occured....");
+        } catch (Exception e) {
+            e.printStackTrace();
+            LOG.error(e);
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+            String stackTrace = sw.toString();
+            LOG.error("Exception - " + stackTrace);
+            response = stackTrace;
+        }
+
+        return response;
+    }
+}
